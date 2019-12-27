537cda19d6fe2c186985ed8d0337be31018f90e2
Midoris-Alva <midorisvargas12@gmail.com>
2019-12-27 13:34:42 -0600

    Soy el bicho hdtpm

---
+++ JS/clicked.js
@@ -0,0 +1,13 @@
+
+let nom1 = prompt("Dame tu nombre");
+let nombre = prompt("Dame tu nombre");
+let edad = parseInt(prompt("Dame tu edad"));
+let altura = parseFloat(prompt("Dame tu altura"));
+let peso = parseInt(prompt("Dame tu peso"));
+
+
+
+let IMC = peso/(altura*altura);
+
+alert("Tú índice de masa corporal es: " + IMC);
+
